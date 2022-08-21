4bb223a644e63487b36cab6baa1fbee7f6e3df6a
Uheezal <aleemabdulmuheez@gmail.com>
2022-08-21 10:51:58 +0100

    finishing touches

--- src/Quote.js
+++ src/Quote.js
@@ -1,34 +1,35 @@
 import axios from 'axios';
-import React,{useEffect, useState} from 'react'
+import React,{useCallback,useEffect,useState} from 'react'
 
 const  Quote = () => {
 const [quote, setQuote] = useState();
+
 const url = 'https://api.adviceslip.com/advice';
 
 
-    useEffect( ()=>{
-    
-        axios.get(url)
-        .then((res) =>{
-            const {data} = res.data.slip.advice;
-            setQuote(data);
-        })
-       .catch((err) =>{
-        console.log(err.message);
-       })
-    
-       
-    
-        
-}
 
-)
+const getAdvice = useCallback(()=>{
+    axios.get(url)
+    .then((res) =>{
+       const   dat= res.data.slip.advice;
+        setQuote(dat);
+        
+        
+    })
+   .catch((err) =>{
+    console.log(err.message);
+   })
+},[setQuote])
+
+useEffect(()=>{
+    getAdvice();
+    },[getAdvice])
 
 
   return (
     <div className='qoute'>
     <h2>{quote}</h2>
-    <button onClick={setQuote(`${data}`)}>Give me advice</button>
+    <button onClick={getAdvice}>Give me advice</button>
     </div>
   )
 }
